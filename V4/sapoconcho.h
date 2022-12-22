e295fedef34322b8792aaa65f76010bdcc297d7a
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2022-12-22 15:27:29 +0100

    Create sapoconcho.h

---
+++ V4/sapoconcho.h
@@ -0,0 +1,12 @@
+#ifndef sapoconcho_h
+#define sapoconcho_h
+
+class sapoconcho {
+  public:
+    sapoconcho();
+    void brake(int t);
+    void drive(int l, int r, int t);
+    long sontar(int trigger);
+}; 
+
+#endif
